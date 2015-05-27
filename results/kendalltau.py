b6c0eaadcb8b2a9aff7502e5e9e48338a0c20398
JaewookByun <bjw0829@gmail.com>
2015-05-27 15:00:47 +0900

    Apply Kendall's rank order distance

---
+++ results/kendalltau.py
@@ -0,0 +1,46 @@
+'''
+Created on 2015. 5. 27.
+
+@author: jack
+'''
+import sys
+import scipy.stats
+import numpy
+import math
+
+def run_main(cfile, sfile, rfile):
+    
+    cf = open(cfile, "r")
+    sf = open(sfile, "r")
+    rf = open(rfile, "a")
+    
+    tot_tau = 0
+    idx = 0
+    while 1:
+        cline = cf.readline()
+        if not cline: break
+        clist1 = cline.split(',')[1]
+        clist = clist1.split()
+        sline = sf.readline()
+        slist1 = sline.split(',')[1]
+        slist = slist1.split()
+        clen = len(clist)
+        slen = len(slist)
+        if clen > slen:
+            clist = clist[0:slen]
+        elif slen > clen:
+            slist = slist[0:clen]
+        tau, p_value = scipy.stats.kendalltau(clist, slist)
+        if not numpy.isnan(tau):
+            tot_tau += math.fabs(tau)
+            print tot_tau
+        idx = idx + 1
+    print tot_tau
+    result = cfile + "\t" + str(tot_tau) + "\n"
+    # rf.write(result)
+
+if __name__=='__main__':
+    if len(sys.argv) != 3:
+        print >> sys.stderr, 'usage: <file to be compared> <result_file_name>'
+    else:
+        run_main(sys.argv[1], "testSolution.csv", sys.argv[2])
